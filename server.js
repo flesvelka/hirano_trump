29d6ec2a6082f9add7dd3b8052796859e1b1c283
fles <bad.apple05@gmail.com>
2013-06-18 23:40:44 +0900

    config.json

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-// httpモジュールの読み込み
-var http = require('http');
-
-// httpサーバーの起動
+var http = require('http')
+  , sprintf = require('sprintf').sprintf;
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(sprintf('%s', 'Hello Node Ninja\n'));
 }).listen(80);
-
-console.log('Server running');
