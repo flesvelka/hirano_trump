f75c3993d047349c238bf598b148386a9e93f031
fles <bad.apple05@gmail.com>
2013-06-18 23:27:01 +0900

    httpサーバ

--- server.js
+++ server.js
@@ -1,31 +1,10 @@
-var fs = require('fs');
-var app = require('http').createServer( function (req, res) {
-    fs.readFile(__dirname + '/game.html', function (err, data) {
-        if (err) return res.writeHead(500);
+// httpモジュールの読み込み
+var http = require('http');
 
-        res.writeHead(200);
-        res.end(data);
-    });
-});
+// httpサーバーの起動
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(80);
 
-var io = require('socket.io').listen(app, { log: false });
-io.sockets.on('connection', function (socket) {
-    socket.on('pulse', function (data) {
-        socket.emit('pulse', data ? data * 2 : 0);
-    });
-});
-
-app.listen(80);
-
-var app = require('express').createServer()
-  , io = require('socket.io').listen(app);
- 
-io.configure('production', function(){
-  io.set('transports', [
-    'websocket',
-    'htmlfile',
-    'xhr-polling'
-  ]);
-});
- 
-app.listen(80);
+console.log('Server running');
