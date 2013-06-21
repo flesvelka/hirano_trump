7331a3f4c07dab0e7f2a83397c01fe21c5c0bf23
fles <s10166ny@edu.tuis.ac.jp>
2013-06-21 17:06:06 +0900

    test

--- server.js
+++ server.js
@@ -1,6 +1,48 @@
-var http = require('http')
-  , sprintf = require('sprintf').sprintf;
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(sprintf('%s', 'Hello Node Ninja\n'));
-}).listen(80);
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'socket.io'
+  });
+});
+
+app.listen(80);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+// Server
+
+var io = require('socket.io').listen(app);
+
+trump = io.of('/game.html').on('connection', function (socket) {
+	socket.json.emit('test', test);
+	socket.on('trump test', function(data) {
+		points.push(data);
+		paint.emit('paint points', data);
+	});
+});
