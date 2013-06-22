1e00a4c6d6327e26bb42579d50d396f2712ae2fb
fles <bad.apple05@gmail.com>
2013-06-22 11:04:07 +0900

    fgs

--- server.js
+++ server.js
@@ -1,52 +1,6 @@
-/**
- * Module dependencies.
- */
-
-console.log("test");
-var express = require('express');
-var app = module.exports = express.createServer();
-console.log("test1");
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-console.log("test2");
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'socket.io'
-  });
-});
-
-app.listen(80);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-
-// Server
-
-var io = require('socket.io').listen(app);
-
-function handler(req, res) {
-	fs.readFile(__dirname + '/game.html', function(err, data) {
-		if (err) {
-			res.writeHead(500);
-			return res.end('Error loading index.html');
-		}
-		res.writeHead(200);
-		res.end(data);
-	});
-}
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+  console.log('request url: ', req.url);
+}).listen(80);
