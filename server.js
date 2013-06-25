ed63f466822831ade67e41d9251b7f3311a28d72
fles <s10166ny@edu.tuis.ac.jp>
2013-06-25 14:37:33 +0900

    httpサーバ game.html表示

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-  console.log('request url: ', req.url);
+http.createServer(function (req, res) { // httpサーバを立てる
+  console.log("server start");
+
+  fs.readFile('game.html', function(err, data) { // ファイルを読み込み、変数dataに代入
+    if (err) { 
+	consle.log("file is not exists");  // ファイルが見つからない
+    }
+    console.log("file read");
+
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'}); // リクエストヘッダ 200 正常に読み込めた
+        res.end(data); // 読み込んだファイルを表示
+    });
+
 }).listen(80);
