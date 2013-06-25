32a7e3e6f383de70e725b53d3691e4daa323fc77
fles <s10166ny@edu.tuis.ac.jp>
2013-06-25 17:41:53 +0900

    tset

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) { // httpサーバを立てる
-  console.log("server start");
 
   fs.readFile('game.html', function(err, data) { // ファイルを読み込み、変数dataに代入
     if (err) { 
@@ -12,7 +11,8 @@ http.createServer(function (req, res) { // httpサーバを立てる
     console.log("file read");
 
     res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'}); // リクエストヘッダ 200 正常に読み込めた
-        res.end(data); // 読み込んだファイルを表示
-    });
-
+    res.end(data); // 読み込んだファイルを表示
+  });
 }).listen(80); // port80でサーバ待機
+
+console.log("server start");
